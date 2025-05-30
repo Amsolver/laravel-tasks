1b5266a9fd86292aa8aec4d33e8ec70b363b22bf
Amsolver <126794787+Amsolver@users.noreply.github.com>
2025-05-31 01:14:29 +0200

    q

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,6 @@
 <?php
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/metric', function () {
-    try {
-        $count = DB::table('agents')->count();
-        return "Número de agentes: {$count}";
-    } catch (\Exception $e) {
-        return "Error al conectar con la base de datos: " . $e->getMessage();
-    }
+    return 'Hola';
 });
