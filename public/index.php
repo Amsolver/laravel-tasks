aedcb4cad9966e0a478091d378f9883ef95f18d2
Amsolver <126794787+Amsolver@users.noreply.github.com>
2025-05-31 11:47:21 +0200

    prueba

--- public/index.php
+++ public/index.php
@@ -1,18 +1,8 @@
 <?php
+if ($_SERVER['REQUEST_URI'] === '/metric') {
+    echo "Hola mundo desde Laravel fallback manual";
+    exit;
+}
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-require __DIR__.'/../vendor/autoload.php';
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+require _DIR_.'/../vendor/autoload.php';
+$app = require_once _DIR_.'/../bootstrap/app.php';
