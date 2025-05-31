ba56b6826c80d56e56b16ccbf64769504a1bc7bd
amahia-telynet <amahia@telynet.com>
2025-05-31 12:57:31 +0200

    prueba routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/app', function () {
+    return 'Hola desde /app';
+});
+
+Route::get('/metric', function () {
+    return 'OK desde /metric';
+});
